4886b119283a7229df2cd7587e8a0ca1846f9dfc
RoyaAlmasimehr <almasimehrroya@gmail.com>
2024-09-05 22:38:50 +0330

    Update CartModel.tsx

--- src/components/CartModel.tsx
+++ src/components/CartModel.tsx
@@ -4,10 +4,11 @@
 
 
 const CartModel =()=>{
-    return(
-        <div>
-cart model
-        </div>
-    )
+    const cartItems=false
+    return (
+      <div className="absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
+       {/* {!cartItems ?():} */}
+      </div>
+    );
 }
 export default CartModel
